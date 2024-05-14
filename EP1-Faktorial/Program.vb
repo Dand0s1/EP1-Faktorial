f999ab6982179a29b7ece1e6605a67d2b432e69a
Daniel Suchý <Suchy.Daniel@stud.vassboskovice.cz>
2024-05-14 09:32:13 +0200

    Ošetření vysoké vstupní hodnoty

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -4,18 +4,20 @@ Module Program
   Sub Main(args As String())
         Dim cislo As Integer
         Dim vypocet As Integer = 1
-
-        Console.WriteLine("Zadej celé èíslo: ")
-        cislo = Console.ReadLine
-        If cislo < 0 Then
-            Console.WriteLine("Musíš zadat celé kladné èíslo")
-        Else For i = 1 To cislo
-                vypocet = vypocet * i
-            Next
-            Console.WriteLine($"Faktoriál {cislo} je {vypocet}")
-            Console.ReadKey()
-        End If
-
+        Do
+            Console.WriteLine("Zadej celé èíslo: ")
+            cislo = Console.ReadLine
+            If cislo < 0 Then
+                Console.WriteLine("Musíš zadat celé kladné èíslo")
+            ElseIf cislo >= 10 Then
+                Console.WriteLine("Musíš zadat èíslo menší než 10")
+            Else For i = 1 To cislo
+                    vypocet = vypocet * i
+                Next
+                Console.WriteLine($"Faktoriál {cislo} je {vypocet}")
+                Console.ReadKey()
+            End If
+        Loop
 
 
     End Sub
